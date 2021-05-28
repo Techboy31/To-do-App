cb95bbf6b35bac25e3b58a59ff93c2d49bbc2c7f
Abhishek <abhishek.singh75@yahoo.com>
2021-05-28 13:48:32 +0530

    react to do list

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import './App.css';
+import Plan from './Plan';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Component } from 'react';
+
+class App extends Component{
+
+  state={
+    items:[],
+    text: ""
+  }
+
+  handleChange =(e)=>{
+
+    this.setState({text: e.target.value})
+  }
+
+  handleAdd =(e) =>{
+
+    if(this.state.text !== "")
+    {
+      const items=[...this.state.items,this.state.text];
+      this.setState({items:items,text:""});
+    }
+  }
+  handleDelete=(id)=>{
+console.log("delete",id);
+const  olditems=[...this.state.items]
+console.log("olditems",olditems);
+const items=olditems.filter((element,i)=>{
+
+return i !==id
+
+})
+console.log("newitems",items);
+this.setState({items:items});
+  }
+  render(){
+  return (
+    <div className="container-fluid my-5">
+      <div className="row">
+        <div className="col-sm-6 mx-auto text-white shadow-lg p-3">
+
+                     <h2 className="text-center">Task Of The Day</h2>
+         
+   <div className="row">
+     
+     <div className="col-9">
+<input type="text" className="form-control" placeholder="write Plan Here" value={this.state.text} onChange={this.handleChange}>
+
+</input>
+
+     </div>
+     <div className="col-2">
+       <button className="btn btn-warning px-5 fw-bold " onClick={this.handleAdd}>Add</button>
+     </div>
+     <div className="container-fluid">
+
+       <ul className="list-unstyled row m-5">
+         
+         {
+           this.state.items.map((value,i) => {
+             return <Plan  key={i} id={i}value={value}  sendData={this.handleDelete}/>
+           })
+         }
+       </ul>
+     </div>
+     </div>      </div>
+        </div>
+ 
+    </div>
+  );
+}
+}
+export default App;
